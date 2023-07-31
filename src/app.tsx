c90b6b62a4b5e998b6314373cd41732cdf047f00
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-07-31 18:40:58 +0900

    [feat] init landing

--- src/app.tsx
+++ src/app.tsx
@@ -2,22 +2,18 @@ import loadable from '@loadable/component';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import tw from 'twin.macro';
 
-import AppProvider from '~/hocs/hoc-provider';
-
-const MainPage = loadable(() => import('./pages/main'));
+const Landing = loadable(() => import('./pages/landing'));
 
 const RouteWrapper = tw.main`relative w-full h-full`;
 const App = () => {
   return (
     <BrowserRouter>
-      <AppProvider>
-        <RouteWrapper>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </RouteWrapper>
-      </AppProvider>
+      <RouteWrapper>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </RouteWrapper>
     </BrowserRouter>
   );
 };
---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,13 @@
+import tw from 'twin.macro';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <div></div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = tw.div``;
+
+export default Landing;
