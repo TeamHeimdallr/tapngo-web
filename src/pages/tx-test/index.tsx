8c10d4afa2ebc567c5fb0c9fef4d880a2ae0108f
lucky7323 <eunho.ambition@gmail.com>
2023-08-01 11:00:02 +0900

    feat: nfc features

--- src/pages/tx-test/index.tsx
+++ src/pages/tx-test/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import tw from 'twin.macro';
 import { parseEther, parseGwei } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
@@ -5,9 +6,41 @@ import { polygonMumbai } from 'viem/chains';
 
 import { ButtonFilled } from '~/components/buttons';
 import { publicClient, walletClient } from '~/configs/setup-contract';
-import { TESTER_PRIVATE_KEY } from '~/constants';
+import { sha256Hash } from '~/utils/string';
 
 const TxTestPage = () => {
+  const [privateKey, setPrivateKey] = useState('');
+
+  const handleNfcReading = async () => {
+    if (typeof NDEFReader === 'undefined') {
+      alert('NFC is not supported in this browser.');
+      return;
+    }
+
+    try {
+      console.log('start');
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      console.log('------');
+
+      ndef.addEventListener('readingerror', () => {
+        console.log('Argh! Cannot read data from the NFC tag. Try another one?');
+      });
+
+      ndef.addEventListener('reading', (event: any) => {
+        const { message, serialNumber } = event;
+        console.log(`> Serial Number: ${serialNumber}`);
+        console.log(`> Records: (${message.records.length})`);
+
+        const pkey = sha256Hash(serialNumber);
+        setPrivateKey(pkey);
+        console.log(pkey);
+      });
+    } catch (error) {
+      console.error('Error while scanning NFC:', error);
+    }
+  };
+
   interface TransferToken {
     from: `0x${string}`;
     to: `0x${string}`;
@@ -33,12 +66,13 @@ const TxTestPage = () => {
 
   return (
     <Wrapper>
+      <ButtonFilled text="start nfc reading" primary="medium" onClick={handleNfcReading} />
       <ButtonFilled
         text="transfer token 0.0001 MATIC"
         primary="medium"
         onClick={() =>
           transferToken({
-            from: TESTER_PRIVATE_KEY as `0x${string}`,
+            from: privateKey as `0x${string}`,
             to: '0x48DBa2D1b6C89Bf8234C2B63554369aDC7Ae3258',
             value: '0.0001',
           })
